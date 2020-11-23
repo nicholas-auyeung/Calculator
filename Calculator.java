b9b76315ce2a068433947e2a213aecb19650accd
Nicholas Auyeung <nicholasauyeung@nicholass-mbp.attlocal.net>
2020-11-23 14:53:05 -0800

    first commit

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package Calculator;
+import java.util.Scanner;
+public class Calculator {
+	
+	private double result;
+	
+	double addition(double firstNum, double secondNum) {
+		return result;
+	}
+	double subtraction(double firstNum, double secondNum) {
+		return result;
+	}
+	double multiplication(double firstNum, double secondNum) {
+		return result;
+	}
+	double division(double firstNum, double secondNum) {
+		return result;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		double firstNum, secondNum;
+	}
+
+}
