626084cf0edf0848aa7a8bef24000aa33eae6d72
Nicholas Auyeung <nicholasauyeung@nicholass-mbp.attlocal.net>
2020-11-23 18:56:41 -0800

    main logic finished

--- Calculator.java
+++ Calculator.java
@@ -2,25 +2,64 @@ package Calculator;
 import java.util.Scanner;
 public class Calculator {
 	
-	private double result;
+	private static double result;
 	
-	double addition(double firstNum, double secondNum) {
-		return result;
+	static void addition(double firstNum, double secondNum) {
+		result = firstNum + secondNum;
+		System.out.println("Result: " + result);
 	}
-	double subtraction(double firstNum, double secondNum) {
-		return result;
+	static void subtraction(double firstNum, double secondNum) {
+		result = firstNum - secondNum;
+		System.out.println("Result: " + result);
 	}
-	double multiplication(double firstNum, double secondNum) {
-		return result;
+	static void multiplication(double firstNum, double secondNum) {
+		result = firstNum * secondNum;
+		System.out.println("Result: " + result);
 	}
-	double division(double firstNum, double secondNum) {
-		return result;
+	static void division(double firstNum, double secondNum) {
+		result = firstNum / secondNum;
+		System.out.println("Result: " + result);
 	}
-	
-	
 	
 	public static void main(String[] args) {
 		double firstNum, secondNum;
+		char operation;
+		boolean moreCalc = true;
+		Scanner scan = new Scanner(System.in);
+		
+		while(moreCalc == true) {
+			System.out.println("Welcome to Calculator" + "\n");
+			System.out.println("Enter any letter to exit");
+			System.out.println("What operation would you like to perform?" + "\n");
+			System.out.println("(1) Addition");
+			System.out.println("(2) Subtraction");
+			System.out.println("(3) Multiplication");
+			System.out.println("(4) Division");
+			operation = scan.next().charAt(0);
+			System.out.println("Enter the two numbers: ");
+			firstNum = scan.nextDouble();
+			secondNum = scan.nextDouble();
+			
+			switch(operation) {
+				case '1':
+					addition(firstNum, secondNum);
+					break;
+				case '2':
+					subtraction(firstNum, secondNum);
+					break;
+				case '3':
+					multiplication(firstNum, secondNum);
+					break;
+				case '4':
+					division(firstNum, secondNum);
+					break;
+				default:
+					System.out.println("Exiting...");
+					moreCalc = false;
+					break;
+			}
+		}
+		
 	}
 
 }
